4661d1a6cd1c95659acc2fe0537f9b0ccb6393c5
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-02-09 23:58:43 +0100

    new commit

--- app.py
+++ app.py
@@ -1,38 +1,104 @@
-from flask import request,render_template,redirect,url_for,Flask
-
-
+from flask import flash, request,render_template,redirect,url_for,Flask
+from datetime import date, timedelta, datetime
+import calendar
+import requests
+from bs4 import BeautifulSoup
 app=Flask(__name__)
-
-
-@app.route('/')
-def get_file():   
-    return render_template('file.html')
-
-
+app.config['SECRET_KEY']="naut1234567"
 
 def descOrder(lists):
+    for r in lists:      
+        r.reverse()
+    return r
+def descSearch(lists):
     lists.reverse()
-    str1 = ''.join(lists)
+    str1=''.join(lists)
     return str1
 
-@app.route('/download')
-def download():
+
+        
+
+@app.route('/search')
+def search():
     date=request.args.get('date').strip() 
     lists=list(date)
-    order=descOrder(lists)
-   
+    order=descSearch(lists)  
     month=list(order[3:5])
     day=list(order[0:2])
     year=list(order[6:10])
-    ordered_day=descOrder(day)
-    ordered_year=descOrder(year)
-    ordered_month=descOrder(month)
+    
+    ordered_day=descSearch(day)
+    ordered_year=descSearch(year)
+    ordered_month=descSearch(month)
+    now=datetime.now()
+    month_name=calendar.month_name[now.month]
+    if int(ordered_day) > now.day or int(ordered_month) > now.month or int(ordered_year) > now.year:
+       flash('Date can not be greater than present date','danger')
+       return redirect(url_for('naut_files')) 
+    return render_template('search.html',day=ordered_day,month=ordered_month,year=ordered_year,month_name=month_name,now=now)
+
+
     
    
 
-    return render_template('download.html',day=ordered_day,month=ordered_month,year=ordered_year)
+
+@app.route('/')
+def naut_files():
+    month = datetime.now().month
+    year = datetime.now().year
+    current_date=datetime.now().day
+    number_of_days = calendar.monthrange(year, month)[1]
+    first_date = date(year, month, 1)
+    last_date = date(year, month, current_date)
+    delta = last_date - first_date
+    a=[(first_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(delta.days + 1)]
+    list=[]
+    list.append(a)
+    order=descOrder(list)
+    now=datetime.now()
+    month_name=calendar.month_name[now.month]
+
+    
+    
+    
+    return render_template('pod.html',order=order,now=now,month=month,month_name=month_name)
 
 
 
+# Web URL
+web_url = "https://polkashots.io/"
+
+# get HTML content
+html = requests.get(web_url).content
+
+# parse HTML Content
+soup = BeautifulSoup(html, "html.parser")
+
+js_files = []
+cs_files = []
+
+for script in soup.find_all("script"):
+	if script.attrs.get("src"):
+		
+		# if the tag has the attribute
+		# 'src'
+		url = script.attrs.get("src")
+		js_files.append(web_url+url)
 
 
+for css in soup.find_all("link"):
+	if css.attrs.get("href"):
+		
+		# if the link tag has the 'href'
+		# attribute
+		_url = css.attrs.get("href")
+		cs_files.append(web_url+_url)
+
+# adding links to the txt files
+with open("javajavascript_files.txt", "w") as f:
+	for js_file in js_files:
+		print(js_file, file=f)
+
+with open("css_files.txt", "w") as f:
+	for css_file in cs_files:
+		print(css_file, file=f)
